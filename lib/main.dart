b8a5be402c6f4aeb8314cb3cb980c07298d292a0
quadroce <francesco_mucci@hotmail.com>
2022-03-10 17:13:48 +0100

    Added images
    UI

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter TimePicker',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        scaffoldBackgroundColor: Colors.grey,
       ),
       home: const MyHomePage(),
     );
@@ -41,14 +42,155 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final player = AudioCache(fixedPlayer: AudioPlayer());
   int checkButton = 0;
-  bool _visible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter TimePicker"),
+        title: const Text("Svegliolino"),
       ),
-      body: Center(
+      body: Column(
+        children: [
+          Image.asset('assets/images/lino.jpg'),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                  icon: const Icon(
+                    Icons.access_alarm_rounded,
+                    size: 100,
+                  ),
+                  /* padding: const EdgeInsets.all(50),
+                  alignment: Alignment.center,
+                  tooltip: 'Increase volume by 10',
+                   */
+                  onPressed: () async {
+                    _selectTime(context);
+                  }),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("${selectedTime.hour}:${selectedTime.minute}"),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () async {
+                      player.fixedPlayer?.stop();
+                      checkButton = 1;
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(360)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 60, vertical: 60),
+                        textStyle: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    child: const Text("Stop")),
+              ),
+              Expanded(
+                  child: ElevatedButton(
+                onPressed: () async {
+                  player.fixedPlayer?.stop();
+                  checkButton = 0;
+                  selectedTime =
+                      selectedTime.add(selectedTime.hour, selectedTime.minute);
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(360)),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 60, vertical: 60),
+                    textStyle: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
+                child: const Text('Snooze'),
+              )),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _selectTime(BuildContext context) async {
+    final TimeOfDay? timeOfDay = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+      initialEntryMode: TimePickerEntryMode.dial,
+    );
+
+    if (timeOfDay != null && timeOfDay != selectedTime) {
+      setState(() {
+        selectedTime = timeOfDay;
+      });
+      checkButton = 0;
+    }
+
+    const tenSec = Duration(seconds: 10);
+    Timer.periodic(tenSec, (Timer t) => checkTimer(selectedTime));
+  }
+
+  late final AudioCache player2;
+  checkTimer(TimeOfDay selectedTime) {
+    int alarmId = 1;
+
+    final DateTime timeSystem;
+    timeSystem = DateTime.now();
+    /* print('time ${timeSystem.hour}' ' + ' ' ${timeSystem.minute}');
+    print('selected ${selectedTime.hour}'
+
+        ' + '
+        ' ${selectedTime.minute}'
+        '+'
+        '$checkButton'); */
+
+    if (timeSystem.hour == selectedTime.hour &&
+        timeSystem.minute == selectedTime.minute &&
+        checkButton == 0) {
+      AndroidAlarmManager.oneShotAt(
+          DateTime(selectedTime.hour, selectedTime.minute),
+          alarmId,
+          //fireAlarm(player));
+          fireAlarm(player));
+      checkButton = 1;
+    }
+  }
+}
+
+fireAlarm(player2) {
+  print('ha funzioneo ${DateTime.now()} +2');
+  //  String audioasset = "assets/sounds/svegliolino.mp3";
+
+  player2.play('sounds/svegliolino.mp3');
+}
+
+extension TimeOfDayExtension on TimeOfDay {
+  TimeOfDay add(int hour, int minute) {
+    Random rnd;
+    int min = 1;
+    int max = 5;
+    rnd = new Random();
+    minute = minute + min + rnd.nextInt(max - min);
+    if (minute >= 60) {
+      minute = minute - 60;
+      hour = hour + 1;
+    }
+
+    return replacing(hour: hour, minute: minute);
+  }
+}
+ //FINO A QUA
+      /* Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -62,6 +204,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       _selectTime(context);
                     }),
                 Text("${selectedTime.hour}:${selectedTime.minute}"),
+              
+              
                 Visibility(
                   visible: true,
                   child: ElevatedButton(
@@ -91,74 +235,4 @@ class _MyHomePageState extends State<MyHomePage> {
             )),
           ],
         ),
-      ),
-    );
-  }
-
-  _selectTime(BuildContext context) async {
-    final TimeOfDay? timeOfDay = await showTimePicker(
-      context: context,
-      initialTime: selectedTime,
-      initialEntryMode: TimePickerEntryMode.dial,
-    );
-
-    if (timeOfDay != null && timeOfDay != selectedTime) {
-      setState(() {
-        selectedTime = timeOfDay;
-      });
-      checkButton = 0;
-    }
-
-    const tenSec = Duration(seconds: 10);
-    Timer.periodic(tenSec, (Timer t) => checkTimer(selectedTime));
-  }
-
-  late final AudioCache player2;
-  checkTimer(TimeOfDay selectedTime) {
-    int alarmId = 1;
-
-    final DateTime timeSystem;
-    timeSystem = DateTime.now();
-    print('time ${timeSystem.hour}' ' + ' ' ${timeSystem.minute}');
-    print('selected ${selectedTime.hour}'
-        ' + '
-        ' ${selectedTime.minute}'
-        '+'
-        '$checkButton');
-
-    if (timeSystem.hour == selectedTime.hour &&
-        timeSystem.minute == selectedTime.minute &&
-        checkButton == 0) {
-      AndroidAlarmManager.oneShotAt(
-          DateTime(selectedTime.hour, selectedTime.minute),
-          alarmId,
-          //fireAlarm(player));
-          fireAlarm(player));
-      checkButton = 1;
-      _visible = true;
-    }
-  }
-}
-
-fireAlarm(player2) {
-  print('ha funzioneo ${DateTime.now()} +2');
-  //  String audioasset = "assets/sounds/svegliolino.mp3";
-
-  player2.play('sounds/svegliolino.mp3');
-}
-
-extension TimeOfDayExtension on TimeOfDay {
-  TimeOfDay add(int hour, int minute) {
-    Random rnd;
-    int min = 1;
-    int max = 5;
-    rnd = new Random();
-    minute = minute + min + rnd.nextInt(max - min);
-    if (minute >= 60) {
-      minute = minute - 60;
-      hour = hour + 1;
-    }
-
-    return replacing(hour: hour, minute: minute);
-  }
-}
+      ) ,*/
